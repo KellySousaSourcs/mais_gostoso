60808baaa670e5ae3512cc4a169ea5a8674a26e9
KellySousaSourcs <kellysousasourcs@gmail.com>
2025-05-26 14:14:49 -0300

    refactor: simplificar o estilo do botão na tela de pagamento em dinheiro

--- lib/screens/PagamentoDinheiroScreen.dart
+++ lib/screens/PagamentoDinheiroScreen.dart
@@ -24,19 +24,14 @@ class PagamentoDinheiroScreen extends StatelessWidget {
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Color(0xFFEDDD1D), // Co
-<<<<<<< HEAD
                   padding: const EdgeInsets.symmetric(
                     vertical: 16,
                     horizontal: 32,
                   ),
-=======
-                  padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
->>>>>>> main
                 ),
                 onPressed: () {
                   Navigator.pushNamed(context, '/pedido/concluido');
                 },
-<<<<<<< HEAD
                 child: const Text(
                   'Confirmar',
                   style: TextStyle(
@@ -46,11 +41,6 @@ class PagamentoDinheiroScreen extends StatelessWidget {
                   ),
                 ),
               ),
-=======
-                child: const Text('Confirmar',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Color(0xFF252810)),
-              ),)
->>>>>>> main
             ],
           ),
         ),
